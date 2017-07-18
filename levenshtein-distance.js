1dbfd357adcf60dcd156b19f56e506a31f3bd9a7
walter_h <hugo.walter@epitech.eu>
2017-07-18 15:34:04 +0200

    v0.2

--- levenshtein-distance.js
+++ levenshtein-distance.js
@@ -10,13 +10,13 @@ function init(i, a, row)
 {
   row = Array(a.length + 1)
   for (i = 0; i <= a.length; i++)
-	{
-		row[i] = i;
-	}
+    {
+	row[i] = i;
+    }
   return row;
 }
 
-function levenshtein(a,b)
+function levenshtein(a, b)
 {
   var i, j, prev, val, row;
   
@@ -24,24 +24,23 @@ function levenshtein(a,b)
 		return error(a, b);
 
   row = init(i, a, row);
-
   for (i = 1; i <= b.length; i++)
+    {
+	prev = i;
+	for (j = 1; j <= a.length; j++)
 	{
-		prev = i;
-		for (j = 1; j <= a.length; j++)
-		{
-			if (b[i-1] === a[j-1])
-				val = row[j-1]; // match
-			else 
-			{
-				val = Math.min(row[j-1] + 1, // substitution
-				Math.min(prev + 1,     // insertion
-                       row[j] + 1));  // deletion
-			}
-		row[j - 1] = prev;
-		prev = val;
-		}
-    row[a.length] = prev;
+	    if (b[i - 1] === a[j - 1])
+		val = row[j - 1];
+	    else 
+	    {
+		val = Math.min(row[j - 1] + 1, // substitution
+			       Math.min(prev + 1,     // insertion
+					row[j] + 1));  // deletion
+	    }
+	    row[j - 1] = prev;
+	    prev = val;
 	}
-  return row[a.length];
+	row[a.length] = prev;
+    }
+    return row[a.length];
 }
